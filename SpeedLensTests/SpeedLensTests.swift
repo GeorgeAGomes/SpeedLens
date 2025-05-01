19f14d73f065e0584e243bdce8159f4dadfb28f2
George <georgevag@protonmail.com>
2025-04-30 23:12:41 -0300

    Add initial project

---
+++ SpeedLensTests/SpeedLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedLensTests.swift
+//  SpeedLensTests
+//
+//  Created by George on 30/04/25.
+//
+
+import Testing
+@testable import SpeedLens
+
+struct SpeedLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
